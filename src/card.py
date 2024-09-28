33d9b99f982153fa2716f07dac235ec8f27c7eed
MitchelSvist <msvistunoff@gmail.com>
2024-09-28 11:50:44 +0300

    card+test_card+readme

--- src/card.py
+++ src/card.py
@@ -1,32 +1,35 @@
 class Card:
     NUMBERS = list(range(105))
-    PENALTY_POINT = [1, 2, 3, 5, 7]
 
-    def __init__(self, number: int, penalty_point: int):
+    def __init__(self, number: int):
         if number not in Card.NUMBERS:
             raise ValueError
-        if penalty_point not in Card.PENALTY_POINT:
-            raise ValueError
         self.number = number
-        self.penalty_point = penalty_point
 
     def __repr__(self):
-        return f'{self.number}_{self.penalty_point}'
+        return f'{self.number}'
 
     def __eq__(self, other):
-        return self.number == other.number and self.penalty_point == other.penalty_point
+        return self.number == other.number
 
+    def score(self) -> int:
+        if self.number == 55:
+            return 7
+        elif self.number % 11 == 0:
+            return 5
+        elif self.number % 10 == 0:
+            return 3
+        elif self.number % 5 == 0:
+            return 2
+        else:
+            return 1
 
-    # def score(self) -> int:
-
-    # def can_play_on(self, other) -> bool:
-    #     """Можно ли играть карту self на карту other."""
-    #     return self.color == other.color or self.number == other.number
+    def can_play_on(self, other) -> bool:
+        """Можно ли играть карту self на карту other."""
+        return self.number < other.number
 
     def save(self):
         return repr(self)
 
     def load(text: str):
-        """From 'y3' to Card('y', 3)."""
-        text = text.split("_")
-        return Card(number=int(text[0]), penalty_point=int(text[1]))
+        return Card(number=int(text))
--- test/test_card.py
+++ test/test_card.py
@@ -1,35 +1,59 @@
 from src.card import Card
 
 def test_init():
-    c = Card(55, 7)
+    c = Card(55)
     assert c.number == 55
-    assert c.penalty_point == 7
 
-# def test_play_on():
-#     c1 = Card.load('y1')
-#     c2 = Card.load('y5')
-#     c3 = Card.load('g1')
-#     c4 = Card.load('g6')
-#
-#     assert c1.can_play_on(c1)
-#     assert c2.can_play_on(c1)
-#     assert c1.can_play_on(c2)
-#     assert c3.can_play_on(c1)
-#     assert not c4.can_play_on(c1)
+def test_repr():
+    c = Card(55)
+    assert c.__repr__() == '55'
+    assert Card.__repr__(Card(60)) == '60'
+
+def test_eq():
+    a = Card(10)
+    c = Card(78)
+    b = Card(78)
+    d = Card(78)
+
+    assert c == b
+    assert c == d
+    assert c == Card(78)
+    assert c != a
+    assert c != Card(11)
+
+def test_score():
+    c = Card(70)
+    assert 3 == c.score()
+    c = Card(55)
+    assert 7 == c.score()
+    c = Card(11)
+    assert 5 == c.score()
+
+def test_play_on():
+    c1 = Card.load('1')
+    c2 = Card.load('55')
+    c3 = Card.load('11')
+    c4 = Card.load('104')
+
+    assert not c1.can_play_on(c1)
+    assert not c2.can_play_on(c1)
+    assert c1.can_play_on(c2)
+    assert not c3.can_play_on(c1)
+    assert c1.can_play_on(c4)
 
 def test_save():
-    c = Card(30, 3)
-    assert repr(c) == "30_3"
-    assert c.save() == "30_3"
+    c = Card(30)
+    assert repr(c) == "30"
+    assert c.save() == "30"
 
-    c = Card(55, 7)
-    assert repr(c) == "55_7"
-    assert c.save() == "55_7"
+    c = Card(55)
+    assert repr(c) == "55"
+    assert c.save() == "55"
 
 def test_load():
-    s = "20_3"
+    s = "20"
     c = Card.load(s)
-    assert c == Card(20, 3)
-    s = "5_1"
+    assert c == Card(20)
+    s = "5"
     c = Card.load(s)
-    assert c == Card(5, 1)
+    assert c == Card(5)
