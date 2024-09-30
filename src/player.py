0fcd9cc7b965cb147aa7f2fe7240d4a0fd0051d5
MitchelSvist <msvistunoff@gmail.com>
2024-09-30 22:50:36 +0300

    +player, +test_player

---
+++ src/player.py
@@ -0,0 +1,35 @@
+import json
+import typing
+from src.hand import Hand
+
+
+class Player:
+    def __init__(self, name: str, hand: Hand, score: int = 0):
+        self.name = name
+        self.hand = hand
+        self.score = score
+
+    def __str__(self):
+        return f'{self.name}({self.score}): {self.hand}'
+
+    def __eq__(self, other: typing.Self | str | dict):
+        if isinstance(other, str):
+            other = self.load(json.loads(other))
+        if isinstance(other, dict):
+            other = self.load(other)
+        return self.name == other.name \
+            and self.score == other.score \
+            and self.hand == other.hand
+
+    def loser(self):
+        '''Проверка на проигрыш игрока'''
+        return self.score >= 66
+
+    def save(self) -> dict:
+        return {'name': self.name,
+                'hand': str(self.hand),
+                'score': self.score}
+
+    @classmethod
+    def load(cls, data: dict):
+        return cls(name=data['name'], hand=Hand.load(data['hand']), score=int(data['score']))
---
+++ test/test_player.py
@@ -0,0 +1,43 @@
+from src.hand import Hand
+from src.player import Player
+
+
+def test_init():
+    h = Hand.load('55 5 95')
+    p = Player(name='Mitchel', hand=h, score=11)
+    assert p.name == 'Mitchel'
+    assert p.hand == h
+    assert p.score == 11
+
+def test_str():
+    h = Hand.load('55 5 95')
+    p = Player(name='Mitchel', hand=h, score=11)
+    assert str(p) == 'Mitchel(11): 55 5 95'
+
+def test_loser():
+    h = Hand.load('55 5 95')
+    p = Player(name='Mitchel', hand=h, score=65)
+    assert not p.loser()
+
+    p = Player(name='Mitchel', hand=h, score=66)
+    assert p.loser()
+
+
+def test_eq():
+    h1 = Hand.load('55 5 95')
+    h2 = Hand.load('55 5 95')
+    p1 = Player(name='Mitchel', hand=h1, score=11)
+    p2 = Player(name='Mitchel', hand=h2, score=11)
+    assert p1 == p2
+
+def test_save():
+    h = Hand.load('55 5 95')
+    p = Player(name='Mitchel', hand=h, score=11)
+    assert p.save() == {'name': 'Mitchel', 'score': 11, 'hand': '55 5 95'}
+
+def test_load():
+    data = {'name': 'Mitchel', 'score': 11, 'hand': '55 5 95'}
+    h = Hand.load('55 5 95')
+    p_expected = Player(name='Mitchel', hand=h, score=11)
+    p = Player.load(data)
+    assert p == p_expected
